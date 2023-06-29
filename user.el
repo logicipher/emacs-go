e934a446a700f2e7f777ac52c0ca73254447b7ef
logicipher <lizpboy@gmail.com>
2023-06-30 00:24:53 +0800

    feat:add window operation key map and customize compilation window

--- user.el
+++ user.el
@@ -2,7 +2,8 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (column-number-mode)
-(global-display-line-numbers-mode t)
+;; (global-display-line-numbers-mode t)
+(set-face-attribute 'default (selected-frame) :height 150)
 (setq inhibit-startup-message t
       cursor-type 'bar)
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -33,7 +34,7 @@
  :ensure t
  :config
  (setq doom-themes-enable-bold t
-	doom-themes-enable-italic t)
+        doom-themes-enable-italic t)
  (load-theme 'doom-nord t))
 ;(use-package doom-modeline
 ;  :ensure t
@@ -65,7 +66,7 @@
   :init
   (projectile-mode +1)
   :bind (:map projectile-mode-map
-	      ("C-c p" . projectile-command-map)))
+              ("C-c p" . projectile-command-map)))
 
 (use-package smerge-mode
   :init (setq smerge-command-prefix "\C-cm"))
@@ -88,11 +89,13 @@
   :init
   (setq lsp-keymap-prefix "C-c l")
   :hook ((c++-mode . lsp-mode)
-	 (elisp-mode . lsp-mode)
-	 (lsp-mode . lsp-enable-which-key-integration))
+         (c-mode . lsp-mode)
+         ;; (elisp-mode . lsp-mode)
+         ;; (emacs-lisp-mode . lsp-mode)
+         (lsp-mode . lsp-enable-which-key-integration))
   :commands (lsp lsp-deferred)
   :config
-  (setq lsp-clients-clangd-args '("--background-index" "--header-insertion=never")))
+  (setq lsp-clients-clangd-args '("-j=8" "--background-index" "--header-insertion=never" "--all-scopes-completion" "--clang-tidy" "--function-arg-placeholders" "--pch-storage=memory")))
 
 (use-package lsp-ui
   :config
@@ -103,9 +106,14 @@
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
   :commands lsp-ui-mode
   ;:bind (:map lsp-ui-mode-map
-	;      ([remap xref-find-references] . lsp-ui-peek-find-references)
-	;      ([remap xref-find-definitions] . lsp-ui-peek-find-definitions))
-)
+        ;      ([remap xref-find-references] . lsp-ui-peek-find-references)
+        ;      ([remap xref-find-definitions] . lsp-ui-peek-find-definitions))
+  )
+
+(use-package flycheck
+  :ensure t
+  :hook ((c++-mode . flycheck-mode)
+	 (c-mode . flycheck-mode)))
 
 
 ;;(use-package corfu
@@ -118,7 +126,7 @@
 (use-package company
   :after lsp-mode
   :bind (:map company-active-map
-	 ("<tab>" . company-compelete-common-or-cycle))
+         ("<tab>" . company-compelete-common-or-cycle))
   :init
   (global-company-mode))
 
@@ -126,3 +134,42 @@
   :ensure t
   :mode ("README\\.md\\'" . gfm-mode)
   :init (setq markdown-command "pandoc"))
+
+;;
+;; some hooks
+;;
+
+;; auto complete braces pair
+(add-hook 'prog-mode-hook #'electric-pair-mode)
+
+(defcustom lc-window-command-prefix (kbd "C-x w")
+  "Prefix for window operation commands."
+  :type 'string
+  :group 'lc)
+(defvar lc-window-cmd-help-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<left>") #'shrink-window-horizontally)
+    (define-key map (kbd "<right>") #'enlarge-window-horizontally)
+    (define-key map (kbd "<up>") #'enlarge-window)
+    (define-key map (kbd "<down>") #'shrink-window)
+    (define-key map (kbd "h") #'windmove-left)
+    (define-key map (kbd "j") #'windmove-down)
+    (define-key map (kbd "k") #'windmove-up)
+    (define-key map (kbd "l") #'windmove-right)
+    map)
+  "Keymap for self-use window commands after `lc-window-command-prefix'")
+(fset 'lc-window-cmd-help-map lc-window-cmd-help-map)
+(global-set-key lc-window-command-prefix lc-window-cmd-help-map)
+
+;; set compilation window property
+(setq compilation-window-height 10)
+(setq compilation-scroll-output 'first-error)
+(defun lc-compilation-hook ()
+  "Make compilation window spilted vertically"
+  (progn
+    (if (not (get-buffer-window "*compilation*"))
+        (progn
+          (split-window-vertically)
+          ))
+    ))
+(add-hook 'compilation-mode-hook 'lc-compilation-hook)
